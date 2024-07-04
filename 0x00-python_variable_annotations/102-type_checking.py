e7059dd77cd122b5ecf916bc548c13d6662ff691
Obada-Badee <obadabadee3@hotmail.com>
2024-07-04 23:02:21 +0300

    Task 12: Initial solution

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Type Checking module """
+from typing import Tuple, List, TYPE_CHECKING
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+if (not TYPE_CHECKING):
+    array = [12, 72, 91]
+
+    zoom_2x = zoom_array(array)
+
+    zoom_3x = zoom_array(array, 3.0)
