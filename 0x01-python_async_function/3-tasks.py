9ae9f1c1712ff053ed87047c86f24bb1c20ef243
Obada-Badee <obadabadee3@hotmail.com>
2024-07-13 03:34:36 +0300

    Fix pycode style on 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
 import asyncio
-
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay) -> asyncio.Task:
     """Create a task object"""
