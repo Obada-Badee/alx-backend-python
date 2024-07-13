9c7c84430cb14b764f21375f0890ab2a281f2f43
Obada-Badee <obadabadee3@hotmail.com>
2024-07-13 05:13:39 +0300

    Use comperhention

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,12 +4,12 @@ import asyncio
 import time
 
 
-ascomp = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """ coroutine that should measure the total runtime and return it."""
-    s = time.perf_counter()
-    await asyncio.gather(ascomp(), ascomp(), ascomp(), ascomp())
-    elapsed = time.perf_counter() - s
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    elapsed = time.perf_counter() - start
     return elapsed
